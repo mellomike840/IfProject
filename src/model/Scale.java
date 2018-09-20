0e71e1463bf8dc1c3d29f479e1eee95b86cf70d5
Alex Rottlaender <AlexRottlaender@gmail.com>
2018-09-20 12:03:18 -0600

    added 2 constructors in the model

--- src/model/Scale.java
+++ src/model/Scale.java
@@ -3,10 +3,28 @@ package model;
 public class Scale
 {
 	//declaration section
+	//data member section 
+	//also called instance variables
+	//default values:
+	//all numbers get 0
+	//boolean =>false
+	//objects = null
 	private String scaleName;
 	private String scaleDegrees;
 	private int degreeCount;
 	
+	
+	public Scale()
+	{
+		//default Constructor
+	}
+	
+	public Scale(String scaleName, String scaleDegrees, int degreeCount)
+	{
+		this.scaleName = scaleName;
+		this.scaleDegrees = scaleDegrees;
+		this.degreeCount = degreeCount;
+	}
 	
 	
 	
